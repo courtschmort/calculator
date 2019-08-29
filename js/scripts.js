d2eebf5068c447f47bb9ca7e122dbad1395bd25a
Courtney Schild and Gabe Monteiro <courtney@>
2019-08-28 20:34:43 -0700

    create calculator with add, subtract, multiply, divide and bmi functionality

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number1, number2));
+
+var bmi = function(number1, number2) {
+  return ((number1 / number2 / number2) * 703);
+};
+
+var number1 = parseInt(prompt("Enter your weight in lbs:"));
+var number2 = parseInt(prompt("Enter your height in in:"));
+
+alert(bmi(number1, number2));
